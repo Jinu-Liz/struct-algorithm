c5e0b94e61ae2f1b94de74928b0ce5f44ddb4511
Hyungseock-Yun <bagzein@gmail.com>
2024-04-20 19:43:03 +0900

    - 연결 리스트(Linked List) > 연결 리스트 구현

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -10,15 +10,83 @@ if __name__ == '__main__':
             self.head = None
 
         def append(self, node):
-            if self.head == None:
+            if self.head is None:
                 self.head = node
 
             else:
                 temp = self.head
-                while temp.next != None:
+                while temp.next is not None:
                     temp = temp.next
 
                 temp.next = node
+
+        def print(self):
+            temp = self.head
+            while temp is not None:
+                print(temp.data, '-> ', end='')
+                temp = temp.next
+
+            print('None')
+
+        def unpacked_print(self):
+            temp = self.head
+            while temp is not None:
+                print(*temp.data.keys(), '-> ', end='')
+                temp = temp.next
+            print('None')
+
+        def find(self, key):
+            temp = self.head
+            while temp is not None:
+                if key in temp.data.keys():
+                    break
+
+                temp = temp.next
+
+            return temp
+
+        def pop(self):  # 마지막(tail) node 꺼내기
+            if self.head is None:
+                return None
+
+            prev = self.head
+            temp = self.head
+            while temp.next is not None:
+                prev = temp
+                temp = temp.next
+
+            if prev is temp:
+                self.head = None
+
+            else:
+                prev.next = None
+
+            return temp
+
+        # pop_pos 기능 추가하기
+        def pop_pos(self, pos):
+            if self.head is None:   # head 없으면 None
+                return None
+
+            if pos < 0:   # 매개변수로 들어온 위치값이 0보다 작으면 None
+                return None
+
+            temp = self.head
+            if pos == 0:
+                self.head = temp.next
+                return temp
+
+            prev = temp
+            temp_pos = 0
+            while temp.next is not None:
+                prev = temp
+                temp = temp.next
+                temp_pos += 1
+                if temp_pos == pos:
+                    prev.next = temp.next
+                    return temp
+
+            return None
 
     node = Node(15)
     print(node.data)
@@ -31,6 +99,38 @@ if __name__ == '__main__':
     print(node.next.data)
 
     linkedList = LinkedList()
-    linkedList.head = node
-    print(linkedList.head.data)
-    print(linkedList.head.next.data)
+    linkedList.print()
+
+    linkedList.append(Node(1))
+    linkedList.print()
+
+    linkedList.append(Node(2))
+    linkedList.print()
+
+    # 꽃말 링크드 리스트 구현
+    flower_list = LinkedList()
+    flower_list.print()
+
+    flower_list.append(Node({'구기자': '희생'}))
+    flower_list.append(Node({'꽈리': '약함, 수줍음'}))
+    flower_list.append(Node({'과꽃': '변화, 추억'}))
+    flower_list.print()
+    flower_list.unpacked_print()
+
+    flower_node = flower_list.find('꽈리')
+    print(flower_node.data.keys())
+    print(flower_node.data.values())
+    print(*flower_node.data.keys(), ':', *flower_node.data.values())
+
+    # pop함수 테스트
+    print(flower_list.pop().data)
+    print(flower_list.pop().data)
+    print(flower_list.pop().data)
+
+    # pop_pos함수 테스트
+    flower_list.append(Node({'구기자': '희생'}))
+    flower_list.append(Node({'꽈리': '약함, 수줍음'}))
+    flower_list.append(Node({'과꽃': '변화, 추억'}))
+
+    flower_node = flower_list.pop_pos(3)
+    flower_list.print()

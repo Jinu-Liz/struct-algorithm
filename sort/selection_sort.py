a2f216a3200694b8d339a10494ff675b556f82e5
Hyungseock-Yun <bagzein@gmail.com>
2024-04-22 23:36:12 +0900

    - 정렬 알고리즘 > 선택 정렬(Selection Sort)

--- sort/selection_sort.py
+++ sort/selection_sort.py
@@ -5,10 +5,10 @@ print(V)
 
 for i in range(len(V) - 1):     # 0부터 마지막 전까지
     min = i
-    for j in range(i + 1, len(V)):
-        if V[j] < V[min]:
+    for j in range(i + 1, len(V)):  # 다음 숫자부터 마지막 전까지
+        if V[j] < V[min]:   # 최소값을 임시 변수에 담아둠
             min = j
 
-    V[i], V[min] = V[min], V[i]
+    V[i], V[min] = V[min], V[i]     # 최소값과 자리 변경
 
 print(V)
